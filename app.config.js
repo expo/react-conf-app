e3bd81000f7311036c3b75740a52b7733dda7723
Kadi Kraman <kadi@expo.io>
2024-11-12 10:54:29 +0000

    Add a fallback splash screen

--- app.config.js
+++ app.config.js
@@ -48,6 +48,11 @@ export default {
     scheme: "reactconfapp",
     assetBundlePatterns: ["**/*"],
     newArchEnabled: true,
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#051726",
+    },
     ios: {
       supportsTablet: true,
       bundleIdentifier: getAppId(),
