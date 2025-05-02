5adbad08b33fda243f6a1c3d2a10dfb404550ab9
Kadi Kraman <kadi@expo.io>
2025-05-02 11:28:50 +0100

    Update eslint

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-// https://docs.expo.dev/guides/using-eslint/
-module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier", "eslint-plugin-react-compiler"],
-  rules: {
-    "prettier/prettier": "error",
-    "react-compiler/react-compiler": "error",
-  },
-};
--- components/TabBarButton.tsx
+++ components/TabBarButton.tsx
@@ -46,6 +46,7 @@ export function TabBarButton({
         onPress?.(e);
       }}
       onPressIn={() => {
+        // eslint-disable-next-line react-compiler/react-compiler
         scale.value = withSpring(0.92);
       }}
       onPressOut={() => {
