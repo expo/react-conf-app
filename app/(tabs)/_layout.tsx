7212e76e4a7a739f61d542bdce74e944e3429c5b
betomoedano <betomoedano@outlook.com>
2025-09-16 11:32:50 -0500

    wip migrating to native tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,13 +2,21 @@ import Feather from "@expo/vector-icons/build/Feather";
 import Ionicons from "@expo/vector-icons/build/Ionicons";
 import Octicons from "@expo/vector-icons/build/Octicons";
 import MaterialCommunityIcons from "@expo/vector-icons/build/MaterialCommunityIcons";
-import { Tabs } from "expo-router";
+import {
+  Badge,
+  Icon,
+  Label,
+  NativeTabs,
+  VectorIcon,
+} from "expo-router/unstable-native-tabs";
 import React from "react";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 import { TabBarButton } from "@/components/TabBarButton";
 import { ThemedText, useThemeColor } from "@/components/Themed";
 import { theme } from "@/theme";
 import { useBookmarkStore } from "@/store/bookmarkStore";
+import { Platform } from "react-native";
 
 export default function TabLayout() {
   const tabBarBackgroundColor = useThemeColor({
@@ -26,77 +34,55 @@ export default function TabLayout() {
     dark: `rgba(255, 255, 255, 0.35)`,
   });
 
+  const bookmarks = useBookmarkStore((state) => state.bookmarks);
+  const hasBookmarks = bookmarks.length > 0;
+
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor,
-        tabBarInactiveTintColor,
-        tabBarStyle: {
-          backgroundColor: tabBarBackgroundColor,
-        },
-      }}
+    <NativeTabs
+      tintColor={tabBarActiveTintColor}
+      backgroundColor={tabBarBackgroundColor}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          headerShown: false,
-          tabBarButton: (props) => <TabBarButton {...props} />,
-          tabBarIcon: ({ color }) => (
-            <Feather size={24} name="calendar" color={color} />
+      <NativeTabs.Trigger name="index">
+        {Platform.select({
+          ios: <Icon sf={"calendar"} />,
+          android: (
+            <Icon src={<VectorIcon family={Feather} name="calendar" />} />
           ),
-        }}
-      />
-      <Tabs.Screen
-        name="bookmarks"
-        options={{
-          headerStyle: {
-            backgroundColor: tabBarBackgroundColor,
-          },
-          headerTitle: () => (
-            <ThemedText fontSize={20} fontWeight="bold">
-              Bookmarked sessions
-            </ThemedText>
+        })}
+        <Label>Calendar</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="bookmarks">
+        {Platform.select({
+          ios: <Icon sf={"bookmark"} />,
+          android: (
+            <Icon
+              src={
+                <VectorIcon family={MaterialCommunityIcons} name={"bookmark"} />
+              }
+            />
           ),
-          tabBarButton: (props) => <TabBarButton {...props} />,
-          tabBarIcon: ({ color }) => <BookmarkIcon color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="speakers"
-        options={{
-          headerStyle: {
-            backgroundColor: tabBarBackgroundColor,
-          },
-          headerShown: false,
-          headerTitle: () => (
-            <ThemedText fontSize={20} fontWeight="bold">
-              Speakers
-            </ThemedText>
+        })}
+        <Label>Bookmarked</Label>
+        {hasBookmarks && <Badge>{bookmarks.length.toString()}</Badge>}
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="speakers" role="search">
+        {Platform.select({
+          ios: <Icon sf={"person.2"} />,
+          android: (
+            <Icon src={<VectorIcon family={Ionicons} name={"people"} />} />
           ),
-          tabBarButton: (props) => <TabBarButton {...props} />,
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="people" size={24} color={color} />
+        })}
+        <Label>Speakers</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="info">
+        {Platform.select({
+          ios: <Icon sf={"info"} />,
+          android: (
+            <Icon src={<VectorIcon family={Octicons} name={"info"} />} />
           ),
-        }}
-      />
-      <Tabs.Screen
-        name="info"
-        options={{
-          headerStyle: {
-            backgroundColor: tabBarBackgroundColor,
-          },
-          headerTitle: () => (
-            <ThemedText fontSize={20} fontWeight="bold">
-              Info
-            </ThemedText>
-          ),
-          tabBarButton: (props) => <TabBarButton {...props} />,
-          tabBarIcon: ({ color }) => (
-            <Octicons size={24} name="info" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+        })}
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
 
