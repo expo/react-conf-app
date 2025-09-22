ee52c7cabc41d5721b60f313d53cc10ce3cc9148
aleqsio <mikucki@gmail.com>
2025-09-22 02:29:16 -0700

    Update shader with brand colors.

--- src/app/talk/[talkId].tsx
+++ src/app/talk/[talkId].tsx
@@ -34,22 +34,22 @@ const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView);
 const source = Skia.RuntimeEffect.Make(`
 uniform float sheetAnim;
 uniform vec2 size;
-float rand(vec2 co){
-    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
-}
+
 vec4 main(vec2 pos) {
   vec2 normalized = pos/vec2(256);
   vec2 offset;
   float dist;
   offset = (pos - vec2(size.x/2, -size.y));
   dist = sqrt(pow(offset.x, 2.0) + pow(offset.y, 2.0)) / sqrt(pow(size.x/2, 2.0) + pow(size.y/2, 2.0));
-  float mixVal = 0;
-  vec4 colorA;
   float anim = 1 - sheetAnim;
-    mixVal = anim;
-     colorA = vec4(normalized.y, normalized.x, 1.0,1.0);
-  vec4 colorB = vec4(0.0, 0.0, 0.0,0.0);
-  vec4 color = mix(colorA, colorB, mixVal+rand(pos)/(6.0));
+
+  offset = (pos - vec2(size.x*anim, size.y*anim));
+  dist = sqrt(pow(offset.x, 2.0) + pow(offset.y, 2.0)) / sqrt(pow(size.x/2, 2.0) + pow(size.x/2, 2.0)) - pow(sheetAnim,1.3);
+  float mixVal = max(0.0,dist);
+  vec4 colorA = vec4(0.345, 0.769, 0.863, 1.0) + vec4(1.0, normalized.x, normalized.y,1.0) / 6.0;
+  vec4 colorB = vec4(0.031, 0.494, 0.643, 1.0);
+
+  vec4 color = mix(colorA, colorB, mixVal);
   return vec4(color);
 }`)!;
 
