c93116c03c59b95430e0218221357fb62762db75
Kadi Kraman <hellokadi@gmail.com>
2025-09-22 14:48:07 +0100

    Render day 1 and 2 in separate lists

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,36 +1,25 @@
 import { useScrollToTop } from "@react-navigation/native";
 import { useFocusEffect } from "expo-router";
 import React, { useRef, useState } from "react";
-import { Platform, StyleSheet, ViewToken } from "react-native";
+import { Platform } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 
 import { ActivityCard } from "@/components/ActivityCard";
 import { NotFound } from "@/components/NotFound";
 import { TalkCard } from "@/components/TalkCard";
-import { ThemedText, ThemedView, useThemeColor } from "@/components/Themed";
+import { ThemedView, useThemeColor } from "@/components/Themed";
 import { ConferenceDay } from "@/consts";
 import { useReactConfStore } from "@/store/reactConfStore";
 import { theme } from "@/theme";
-import { Session } from "@/types";
 import { HomeHeader } from "@/components/HomeHeader";
 import { DayPicker } from "@/components/DayPicker";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-type SessionItem =
-  | {
-      type: "session";
-      day: number;
-      item: Session;
-    }
-  | {
-      type: "section-header";
-      day: number;
-    };
-
 export default function Schedule() {
+  // TODO (Kadi): choose day based on the date
+  const [selectedDay, setSelectedDay] = useState(ConferenceDay.One);
   const scrollRef = useRef<FlatList>(null);
   useScrollToTop(scrollRef as any);
-  const [shouldShowDayOneHeader, setShouldShowDayOneHeader] = useState(true);
   const insets = useSafeAreaInsets();
   const sectionListBackgroundColor = useThemeColor(theme.color.background);
 
@@ -40,51 +29,15 @@ export default function Schedule() {
 
   const { dayOne, dayTwo } = useReactConfStore((state) => state.schedule);
   const refreshSchedule = useReactConfStore((state) => state.refreshData);
+  const data = selectedDay === ConferenceDay.One ? dayOne : dayTwo;
 
-  const scrollToSection = (day: ConferenceDay) => {
-    if (day === ConferenceDay.One) {
-      scrollRef.current?.scrollToOffset({
-        offset: 0,
-        animated: true,
-      });
-    } else {
-      scrollRef.current?.scrollToIndex({
-        index: dayOne.length - 1,
-        animated: true,
-        viewOffset: -theme.space16,
-      });
-    }
+  const handleSelectDay = (day: ConferenceDay) => {
+    scrollRef.current?.scrollToOffset({
+      offset: 0,
+      animated: true,
+    });
+    setSelectedDay(day);
   };
-
-  const onViewableItemsChanged = (items: {
-    viewableItems: ViewToken<SessionItem>[];
-    changed: ViewToken<SessionItem>[];
-  }) => {
-    const topVisibleIndex = items.viewableItems?.[0]?.index || 0;
-    const dayTwoStartIndex = dayOne.length - 1;
-    const isDayOneActive = topVisibleIndex < dayTwoStartIndex;
-    const isDayTwoActive = topVisibleIndex >= dayTwoStartIndex;
-
-    if (isDayOneActive && !shouldShowDayOneHeader) {
-      setShouldShowDayOneHeader(true);
-    } else if (isDayTwoActive && shouldShowDayOneHeader) {
-      setShouldShowDayOneHeader(false);
-    }
-  };
-
-  const data = [
-    ...dayOne.map((item) => ({
-      type: "session",
-      day: ConferenceDay.One,
-      item,
-    })),
-    { type: "section-header", day: ConferenceDay.Two },
-    ...dayTwo.map((item) => ({
-      type: "session",
-      day: ConferenceDay.Two,
-      item,
-    })),
-  ] as SessionItem[];
 
   if (!dayOne.length || !dayTwo.length) {
     return <NotFound message="Schedule unavailable" />;
@@ -94,7 +47,6 @@ export default function Schedule() {
     <ThemedView style={{ flex: 1 }}>
       <FlatList
         ref={scrollRef}
-        onViewableItemsChanged={onViewableItemsChanged}
         style={{ backgroundColor: sectionListBackgroundColor }}
         contentContainerStyle={{
           paddingBottom: Platform.select({ android: 100, default: 0 }),
@@ -103,28 +55,13 @@ export default function Schedule() {
         data={data}
         stickyHeaderIndices={[0]}
         ListHeaderComponent={() => (
-          <DayPicker
-            isDayOne={shouldShowDayOneHeader}
-            onSelectDay={scrollToSection}
-          />
+          <DayPicker selectedDay={selectedDay} onSelectDay={handleSelectDay} />
         )}
         renderItem={({ item }) => {
-          if (item.type === "section-header") {
-            return (
-              <ThemedView style={styles.sectionHeader}>
-                <ThemedText fontWeight="bold" fontSize={24}>
-                  Day 2
-                </ThemedText>
-              </ThemedView>
-            );
-          }
-
-          if (item.item.isServiceSession) {
-            return <ActivityCard session={item.item} />;
+          if (item.isServiceSession) {
+            return <ActivityCard session={item} />;
           } else {
-            return (
-              <TalkCard key={item.item.id} session={item.item} day={item.day} />
-            );
+            return <TalkCard key={item.id} session={item} day={selectedDay} />;
           }
         }}
       />
@@ -132,10 +69,3 @@ export default function Schedule() {
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionHeader: {
-    marginBottom: theme.space12,
-    paddingHorizontal: theme.space24,
-  },
-});
