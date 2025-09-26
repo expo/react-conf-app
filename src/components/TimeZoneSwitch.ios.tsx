eef708237b1745890dda69dbaf5055754597be88
Kadi Kraman <hellokadi@gmail.com>
2025-09-26 11:36:29 +0100

    Switch between venue and local tz

--- src/components/TimeZoneSwitch.ios.tsx
+++ src/components/TimeZoneSwitch.ios.tsx
@@ -1,19 +1,34 @@
 import { useReactConfStore } from "@/store/reactConfStore";
+import { getCurrentTimezone } from "@/utils/formatDate";
 
 import { Button, ContextMenu, Host, Picker } from "@expo/ui/swift-ui";
+import * as Haptics from "expo-haptics";
+
+const options = ["PDT (Venue)", `${getCurrentTimezone()} (Local)`];
 
 export function TimeZoneSwitch() {
   const shouldUseLocalTz = useReactConfStore((state) => state.shouldUseLocalTz);
   const toggleLocalTz = useReactConfStore((state) => state.toggleLocalTz);
+
+  const selectedIndex = shouldUseLocalTz ? 1 : 0;
+
+  const handleToggleLocalTz = (newIndex: number) => {
+    if (selectedIndex !== newIndex) {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+      toggleLocalTz();
+    }
+  };
 
   return (
     <Host style={{ width: 33, height: 44 }}>
       <ContextMenu>
         <ContextMenu.Items>
           <Picker
-            options={["PDT", "PST"]}
-            selectedIndex={shouldUseLocalTz ? 0 : 1}
-            onOptionSelected={({ nativeEvent: { index } }) => toggleLocalTz()}
+            selectedIndex={selectedIndex}
+            options={options}
+            onOptionSelected={({ nativeEvent: { index } }) =>
+              handleToggleLocalTz(index)
+            }
           />
         </ContextMenu.Items>
         <ContextMenu.Trigger>
