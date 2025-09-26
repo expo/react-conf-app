e846636a8a98f2601241d7738b67a3ab510114ff
Kadi Kraman <hellokadi@gmail.com>
2025-09-26 15:58:27 +0100

    Improve icon readability

--- src/components/Bookmark.ios.tsx
+++ src/components/Bookmark.ios.tsx
@@ -7,12 +7,14 @@ import { isLiquidGlassAvailable } from "expo-glass-effect";
 import { useThemeColor } from "./Themed";
 import * as Haptics from "expo-haptics";
 import { Platform } from "react-native";
+import { useMemo } from "react";
 
 export function Bookmark({ session }: { session: Session }) {
   const { toggleBookmark, isBookmarked } = useBookmark();
-  const tintColor = useThemeColor({
-    light: theme.colorReactLightBlue,
-    dark: theme.colorReactDarkBlue,
+  const tintColor = useThemeColor(theme.color.reactBlue);
+  const notSelectedIconColor = useThemeColor({
+    dark: theme.colorWhite,
+    light: theme.colorGrey,
   });
 
   const handlePress = async () => {
@@ -28,19 +30,27 @@ export function Bookmark({ session }: { session: Session }) {
   };
 
   const bookmarked = isBookmarked(session.id);
+  const backgroundColor = useThemeColor(theme.color.background);
+
+  const imageColor = useMemo(() => {
+    if (isLiquidGlassAvailable()) {
+      return bookmarked ? theme.colorWhite : notSelectedIconColor;
+    }
+    return tintColor;
+  }, [tintColor, bookmarked, notSelectedIconColor]);
 
   return (
     <HeaderButton
       buttonProps={{
         onPress: handlePress,
         variant: "glassProminent",
-        color: tintColor,
+        color: bookmarked ? tintColor : backgroundColor,
       }}
       imageProps={{
         systemName: bookmarked ? "bookmark.fill" : "bookmark",
         size: isLiquidGlassAvailable() ? 16 : 24,
         modifiers: [frame({ height: 20, width: 10 })],
-        color: isLiquidGlassAvailable() ? "white" : tintColor,
+        color: imageColor,
       }}
     />
   );
